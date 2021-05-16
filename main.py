a692c3e3a593015cb50275173599d3e09bd0bc88
Badri <kbadri01@gmail.com>
2021-05-16 15:57:51 +0530

    commit1

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-#repetitive Addition
+# repetitive Addition
 def rep(m):
-    sum1=0
+    sum1 = 0
     while (m > 0):
         sum1 = int(sum1 + m % 10)
         m = int(m) / 10
     return sum1
-n=int(input("Enter a no"))
-sum=rep(n)
-while(sum>=10):
-    sum=rep(sum)
+
+
+n = int(input("Enter a no"))
+sum = rep(n)
+while(sum >= 10):
+    sum = rep(sum)
 print(sum)
+# done
